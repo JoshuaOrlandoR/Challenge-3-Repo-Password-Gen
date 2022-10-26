59f0fd10a278b39128042146ba5cf530c4a4a3f4
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2022-10-26 03:56:03 -0400

    Third commit - spaced out html for readability, added notes + rearranged + pseudocoded javascript

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-// Assignment code here - <Test Change for Commit>
+// Assignment code here 
+// Variables that will enable code 
+
+
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button  - Sequentially, it makes more sense to have this before the writePassword function
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +22,33 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Below is the function that needs to be written in order to generate the password
+function generatePassword() {
+  // console.log("Button press test")  - tested to see if the button worked, it does
+ 
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TO DO LIST - I was told this is called pseudo coding, and it is good practice 
+  // Prompts for 
+      // Length (8 - 128)
+      // Lowercase
+      // Uppercase
+      // Special/Unique 
+  // Validate input (?) and ensure at least 1 prompt is used
+  // Show generated password in alert or directly on page
