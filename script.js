0819df6722b07961afd2171f1da8bde06d1ff2d0
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2022-10-26 18:55:55 -0400

    Fourth commit - wrote out check functions for each character type - theres one error that needs to be addressed across each of these blocks of code

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Assignment code here 
 // Variables that will enable code 
-
-
-
+var numbers = ["0","1","2","3","4","5","6","7","8","9","0"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var special = ["`","~","!","@","#","$","%","^","&","*","(",")","-","=","+",",",".","<",">","/","?"]
+var pwLength //Used in the lengthCheck function
+var pwNumbers
+var pwLower
+var pwUpper
+var pwSpecial
 
 
 // Get references to the #generate element
@@ -21,6 +27,95 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// There are several functions that check if the user wants certain parameters in their password
+
+//LENGTH CHECK - FINALLY WORKS
+
+function lengthCheck() {
+  pwLength = window.prompt("Please choose (in numbers) how long you would like your password to be. Please select a number between 8 and 128 ");
+  if (pwLength<8) {
+    window.alert("Your password isn't long enough! Please select a number equal to or greater than 8, but equal to or lower than 128");
+  
+  } else if (pwLength>128) {
+    window.alert ("Your password is too large! Please select a number equal to or greater than 8, but equal to or lower than 128");
+   
+  } else if (isNaN(pwLength)) { //Had to research if there was a check for only numbers, testing to see if NaN works here 
+    window.alert ("That isn't a number! Please only input numbers, i.e. 12");
+    
+  }
+    return pwLength;
+}
+
+//Number Check
+function numberCheck() {
+  pwNumbers = window.prompt ("Do you want numbers in your password? Please type yes or no")
+  if (pwNumbers === undefined) {
+    window.alert ("Please only answer using yes or no"); // this isnt working in console currently, have to address it 
+  } else if (pwNumbers === "yes") {
+    pwNumbers = true;
+    return pwLower;
+
+  } else if (pwNumbers === "no") {
+    pwNumbers = false;
+    return pwNumbers
+  }
+  return pwNumbers
+}
+
+
+//LOWER CASE CHECK - Sort of works? True/False return properly but entering a non-yes/no answer doesn't bring up the alert
+
+function lowerCheck() {
+  pwLower = window.prompt("Do you want lowercase letters in your password? Please type yes or no ");
+  if (pwLower === undefined) {
+    window.alert ("Please only answer using yes or no"); // this isnt working in console currently, have to address it 
+  } else if (pwLower === "yes") {
+    pwLower = true;
+    return pwLower;
+
+  } else if (pwLower === "no") {
+    pwLower = false;
+    return pwLower
+  }
+  return pwLower
+}
+
+
+//UPPER CASE CHECK - Copied code for lowerCheck and just changed it to upperCheck - could also rewrite it as a function expression (maybe?)
+
+function upperCheck() {
+  pwUpper = window.prompt("Do you want uppercase letters in your password? Please type yes or no ");
+  if (pwUpper === undefined) {
+    window.alert ("Please only answer using yes or no"); // this isnt working in console currently, have to address it 
+  } else if (pwUpper === "yes") {
+    pwUpper = true;
+    return pwUpper;
+
+  } else if (pwUpper === "no") {
+    pwUpper = false;
+    return pwUpper
+  }
+  return pwUpper
+}
+
+//SPECIAL CHAR CHECK
+
+function specialCheck() {
+  pwSpecial = window.prompt("Do you want special characters in your password? Please type yes or no ");
+  if (pwSpecial === undefined) {
+    window.alert ("Please only answer using yes or no"); // this isnt working in console currently, have to address it 
+  } else if (pwSpecial === "yes") {
+    pwUpper = true;
+    return pwSpecial;
+
+  } else if (pwSpecial === "no") {
+    pwUpper = false;
+    return pwSpecial
+  }
+  return pwSpecial
+} 
+
 
 // Below is the function that needs to be written in order to generate the password
 function generatePassword() {
